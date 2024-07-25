e6c98f8995c533e20446e0f6dc8f9088a75a3d37
Jk9228 <junwei921028@gmail.com>
2024-07-25 17:26:01 +0800

    install mysql-connect-python

--- 神經網路研究/demo/member/views.py
+++ 神經網路研究/demo/member/views.py
@@ -10,15 +10,16 @@ cursor = connection.cursor()
 cursor.execute("use ooschool;")
 
 cursor.execute("SELECT * FROM member ORDER BY age DESC limit 3;")
-
 records = cursor.fetchall()
-for r in records:
-        print(r)
 
-cursor.close()
-connection.close()
+
+
 def show_member_info(request):
+    for members in records:
+        print(members)
     return render(request, "member_page.html" , {
         'member_name': 'jun',
         'member_age': '20'
     })
+cursor.close()
+connection.close()
