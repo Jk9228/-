4be626c2dea581fdc43a94eb0d85ca218563c603
Jk9228 <junwei921028@gmail.com>
2024-07-25 15:55:00 +0800

    2024.07.25 3:54

--- 神經網路研究/demo/member/views.py
+++ 神經網路研究/demo/member/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
-
+import mysql.connector
 # Create your views here.
 
+connection = mysql.connector.connect(host='localhost', port='3306',
+user='Jk9228', password='1234')
+
+cursor = connection.cursor()
+
+cursor.execute("use ooschool;")
+
+cursor.execute("SELECT * FROM member ORDER BY age DESC limit 3;")
+
+records = cursor.fetchall()
+for r in records:
+        print(r)
+
+cursor.close()
+connection.close()
 def show_member_info(request):
     return render(request, "member_page.html" , {
         'member_name': 'jun',
